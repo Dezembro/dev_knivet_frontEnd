de5a0a867b85c46806dfd69e65fcc5d954b449f5
Guilherme Morgado Fonseca <md.softwaredeveloper@gmail.com>
2018-01-08 20:01:17 -0200

    Erro PhP Header

--- resumo.php
+++ resumo.php
@@ -4,12 +4,22 @@
 ?>
 <?php  if(isset($_SESSION['logado'])):    ?>
 
+<?php endif;
+
+if (!($_SESSION['logado'])) {
+    
+   header("location:index.html");
+}
+//header("location:index.html");
+//Warning: Cannot modify header information - headers already sent by (output started at C:\xampp\htdocs\Knivet\resumo.php:86) in C:\xampp\htdocs\Knivet\resumo.php on line 587
+?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="img/apple-icon.pzng" />
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
      <title>Knivet - Seja mais produtivo</title>
@@ -583,8 +593,10 @@ demo = {
 
 </script>
 
-</html>
-<?php endif;
-header("location:index.html");
 
-?>
+
+
+
+
+</html>
+
